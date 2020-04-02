0cc58c56da7428c23f0b01dc6e9ae6d694b29db3
Matt Vertescher <mvertescher@gmail.com>
2020-04-01 22:02:21 -0400

    Update to newt 1.7

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,14 +19,14 @@ $NEWT upgrade
 # Create and build the bootloader
 $NEWT target create nrf52840_boot
 $NEWT target set nrf52840_boot app=@mcuboot/boot/mynewt
-$NEWT target set nrf52840_boot bsp=@apache-mynewt-core/hw/bsp/nrf52840pdk
+$NEWT target set nrf52840_boot bsp=@apache-mynewt-core/hw/bsp/nordic_pca10056
 $NEWT target set nrf52840_boot build_profile=optimized
 $NEWT build nrf52840_boot
 
 # Create and build `btshell`
 $NEWT target create nrf52840_btshell
 $NEWT target set nrf52840_btshell app=@apache-mynewt-nimble/apps/btshell \
-  bsp=@apache-mynewt-core/hw/bsp/nrf52840pdk \
+  bsp=@apache-mynewt-core/hw/bsp/nordic_pca10056 \
   build_profile=optimized
 $NEWT build nrf52840_btshell
 $NEWT create-image nrf52840_btshell 0
